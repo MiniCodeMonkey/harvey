98aaa4149e162689989c5a815e27caa8f29483a8
Mathias Hansen <me@codemonkey.io>
2017-08-28 19:20:38 -0400

    Improved mobile responsiveness and added GA tracking

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -13,26 +13,20 @@
         <link href="//cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/0.4.0/MarkerCluster.css" rel="stylesheet">
         <link href="//cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/0.4.0/MarkerCluster.Default.css" rel="stylesheet">
         <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css" />
+
+        <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-47255883-3', 'auto');
+        ga('send', 'pageview');
+        </script>
     </head>
     <body>
     <div class="container-fluid">
     <i id="loading-indicator" class="fa fa-spin fa-4x fa-refresh"></i>
-
-        <div class="row">
-            <div class="col-md-3">
-                @foreach (App\Emergency::getCached() as $emergency)
-                    <a href="https://twitter.com/{{ $emergency->message->user_handle }}/status/{{ $emergency->message->twitter_id }}" target="
-                    _blank">{{ $emergency->message->message_created->diffForHumans() }}</a>
-
-                    <a href="#{{ $emergency->lat }},{{ $emergency->lng }}" class="marker-link btn btn-default pull-right">
-                        <i class="fa fa-search"></i>
-                    </a>
-                    <blockquote>{{ $emergency->message->message_text }}</blockquote>
-                @endforeach
-            </div>
-            <div id="map" class="col-md-9"></div>
-        </div>
-
         <div class="panel panel-default" id="about">
             <div class="panel-body">
                 <h2>
@@ -52,6 +46,21 @@
                 
                 <p>Questions? Get in touch with us <a href="https://twitter.com/mjwhansen" target="_blank">@mjwhansen</a> or <a href="https://twitter.com/mathiashansen" target="_blank">@mathiashansen</a></p>
             </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3 hidden-xs hidden-sm">
+                @foreach (App\Emergency::getCached()->take(10) as $emergency)
+                    <a href="https://twitter.com/{{ $emergency->message->user_handle }}/status/{{ $emergency->message->twitter_id }}" target="
+                    _blank">{{ $emergency->message->message_created->diffForHumans() }}</a>
+
+                    <a href="#{{ $emergency->lat }},{{ $emergency->lng }}" class="marker-link btn btn-default pull-right">
+                        <i class="fa fa-search"></i>
+                    </a>
+                    <blockquote>{{ $emergency->message->message_text }}</blockquote>
+                @endforeach
+            </div>
+            <div id="map" class="col-md-9 col-xs-12"></div>
         </div>
     </div>
 
