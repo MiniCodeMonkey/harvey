18beb600cd59972c55950e6d185bf6e63c7dfae6
Mathias Hansen <me@codemonkey.io>
2017-08-29 07:49:35 -0400

    Added export to CSV route

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,76 @@ Route::get('/', function () {
 Route::get('emergencies', function () {
 	return App\Emergency::getCached();
 });
+
+Route::get('emergencies.csv', function () {
+	$headers = [
+		'Cache-Control' => 'must-revalidate, post-check=0, pre-check=0',
+		'Content-type' => 'text/csv',
+		'Content-Disposition' => 'attachment; filename=emergencies.csv',
+		'Expires' => '0',
+		'Pragma' => 'public'
+    ];
+
+	$rows = App\Emergency::getCached();
+
+	$csvHeaders = [
+		'id',
+		'lat',
+		'lng',
+		'accuracy_score',
+		'accuracy_type',
+		'street',
+		'city',
+		'zip',
+		'state',
+		'is_safe',
+		'created_at',
+		'updated_at',
+		'message.id',
+		'message.twitter_id',
+		'message.message_created',
+		'message.message_text',
+		'message.user_id',
+		'message.user_handle',
+		'message.user_name',
+		'message.user_location',
+		'message.created_at',
+		'message.updated_at'
+	];
+
+	$out = fopen('php://output', 'w');
+	fputcsv($out, $csvHeaderss);
+
+	$callback = function() use ($rows, $out) {
+		$rows->each(function ($row) use ($out) {
+			fputcsv($out, [
+				$row->id,
+				$row->lat,
+				$row->lng,
+				$row->accuracy_score,
+				$row->accuracy_type,
+				$row->street,
+				$row->city,
+				$row->zip,
+				$row->state,
+				$row->is_safe,
+				$row->created_at,
+				$row->updated_at,
+				$row->message->id,
+				$row->message->twitter_id,
+				$row->message->message_created,
+				$row->message->message_text,
+				$row->message->user_id,
+				$row->message->user_handle,
+				$row->message->user_name,
+				$row->message->user_location,
+				$row->message->created_at,
+				$row->message->updated_at
+			]);
+		});
+
+		fclose($out);
+	};
+
+	return response()->stream($callback, 200, $headers);
+});
